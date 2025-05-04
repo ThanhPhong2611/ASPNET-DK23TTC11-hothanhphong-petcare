5b5a9a18f34e43c31f24615639ca4b9bd874b008
YourName <youremail@example.com>
2025-05-04 14:32:32 +0700

    Add full project

---
+++ src/pet-care/ViewModels/AnimalBookingViewModel.cs.cs
@@ -0,0 +1,13 @@
+﻿// File: /ViewModels/AnimalBookingViewModel.cs
+using PetShopProj.Models;
+
+namespace PetShopProj.ViewModels
+{
+    public class AnimalBookingViewModel
+    {
+        public Animal Animal { get; set; }
+        public Call Booking { get; set; }
+        public AddCommentViewModel Comment { get; set; }
+    }
+}
+
